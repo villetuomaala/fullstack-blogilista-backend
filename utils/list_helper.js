97b983b24384703563f47dd0b0a4c890b6ceb446
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-09-09 23:28:25 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => blogs.length === 0 
+  ? 0 
+  : blogs.map(b => {
+    return b.likes ? b.likes : 0
+  }).reduce((total, val) => total + val)
+
+
+module.exports = { dummy, totalLikes }
