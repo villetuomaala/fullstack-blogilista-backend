a10215956fecc755185eedc466f424b33ca516ac
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-10-21 22:03:47 +0300

    Obfuscate request.body.password

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,8 @@
 const logger = require('./logger')
 
 const requestLogger = (request, response, next) => {
+  if (request.body.password) request.body.password = '***secret***'
+  
   logger.info('Method:', request.method)
   logger.info('Path:  ', request.path)
   logger.info('Body:  ', request.body)
