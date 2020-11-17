f7ea6e376e0e8862ff512f5dc78978066b457ddf
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-11-17 21:16:01 +0200

    4.19: blogilistan laajennus, step7

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,6 +1,16 @@
 const blogController = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const CustomError = require('../utils/customError')
+
+const getTokenFrom = request => {
+  const authorization = request.get('Authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  throw CustomError('Unauthorized', 'Authorization header missing', 401)
+}
 
 blogController.get('/', async (request, response, next) => {
   try {
@@ -13,9 +23,11 @@ blogController.get('/', async (request, response, next) => {
 
 blogController.post('/', async (request, response, next) => {
   try {
-    const user = await User.findById(request.body.userId)
-    delete request.body.userId
+    const token = getTokenFrom(request)
+    const verifiedToken = jwt.verify(token, process.env.TOKEN_KEY)
+    if (!token || !verifiedToken.id) throw CustomError('Unauthorized', 'Invalid token', 401)
 
+    const user = await User.findById(verifiedToken.id)
     const blog = new Blog({
       ...request.body,
       user: user.id
--- models/blog.js
+++ models/blog.js
@@ -21,7 +21,8 @@ const blogSchema = mongoose.Schema({
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
+    required: true
   }
 })
 
